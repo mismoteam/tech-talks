9c5033c526b70cfce04c6192f04b3a8dd628b960
Victor Da Luz <victor@lognllc.com>
2013-05-24 11:09:19 -0600

    BBB Sample: adding some comments

--- backbone-boilerplate-example/js/config.js
+++ backbone-boilerplate-example/js/config.js
@@ -7,7 +7,12 @@ define([
 ], function(){
 
 	var apiBaseUrl = 'https://www.googleapis.com/tasks/v1/'; 
+    
+	// This task list id is from my account, you will need to either 
+	// use your own task list or maybe implement task list selection 
+	// as an excercize
     var taskListId = 'MTA4NjY3ODU1NTA3NjgwMjM0NTk6MTIyMDY1MDUxNzow';
+    
     var authToken = '';
 
     return {
